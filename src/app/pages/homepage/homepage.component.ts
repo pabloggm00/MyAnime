77a8e2ce004aba07e78311b369d06310e8568332
Pablo Gonzalez <pabloggm00@gmail.com>
2024-03-25 00:34:51 +0100

    template iniciado

---
+++ src/app/pages/homepage/homepage.component.ts
@@ -0,0 +1,16 @@
+import { NgStyle } from '@angular/common';
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-homepage',
+  templateUrl: './homepage.component.html',
+  styleUrl: './homepage.component.css'
+})
+export class HomepageComponent {
+
+
+  constructor(){}
+
+
+
+}
---
+++ src/app/shared/footer/footer.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-footer',
+  templateUrl: './footer.component.html',
+  styleUrl: './footer.component.css'
+})
+export class FooterComponent implements OnInit{
+
+  anio: number = new Date().getFullYear();
+
+
+  constructor(){}
+
+  ngOnInit(): void {
+    
+  }
+
+}
