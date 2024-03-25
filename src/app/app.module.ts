59a3b3b425a6b4c868950343166a585c3b468228
Pablo Gonzalez <pabloggm00@gmail.com>
2024-03-25 01:24:29 +0100

    anime details and anime list components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,17 @@ import { AppComponent } from './app.component';
 import { HeaderComponent } from './shared/header/header.component';
 import { FooterComponent } from './shared/footer/footer.component';
 import { HomepageComponent } from './pages/homepage/homepage.component';
+import { AnimeDetailsComponent } from './pages/anime-details/anime-details.component';
+import { AnimeListComponent } from './pages/anime-list/anime-list.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     FooterComponent,
-    HomepageComponent
+    HomepageComponent,
+    AnimeDetailsComponent,
+    AnimeListComponent
   ],
   imports: [
     BrowserModule,
